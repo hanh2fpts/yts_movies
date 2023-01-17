8722e266ae0a3cfd882104ff711a7818e5f2acee
hanh2fpts <hanh2fpts.com.vn>
2023-01-17 16:06:06 +0700

    create project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yts_movies/app.dart';
 
 void main() {
   runApp(const MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:yts_movies/main.dart';
+import 'package:yts_movies/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
