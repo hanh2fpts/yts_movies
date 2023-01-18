11c8995b0065707000a0a95b8faf64ddff4e345a
hanh2fpts <hanh2fpts.com.vn>
2023-01-18 13:17:55 +0700

    them ket noi dio

---
+++ lib/utils/app_constant.dart
@@ -0,0 +1,9 @@
+mixin UrlConstants {
+  static const String urlBase = 'https://yts.mx/';
+}
+mixin GatewayConstant {
+  static const String apiVersionPrefix = 'api/v2/';
+}
+mixin KeyStorageConstant {
+  static const String tokenKey = 'Token';
+}
